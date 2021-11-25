dcb08f879ea259c80fffdb5848480e20884d53c1
Russell Gordon <github@russellgordon.ca>
2021-11-25 14:39:29 -0500

    Initial Commit

---
+++ NameGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameGame
+//
+//  Created by Russell Gordon on 2021-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NameGame/NameGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameGameApp.swift
+//  NameGame
+//
+//  Created by Russell Gordon on 2021-11-25.
+//
+
+import SwiftUI
+
+@main
+struct NameGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
